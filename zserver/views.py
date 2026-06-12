5f0f9f24ac149920ee0e782ac0c821d8d54535c6
Jitendra Kumar <jk69854@gmail.com>
2026-06-12 21:49:36 +0530

    setup Django Rest Framework.

--- zserver/views.py
+++ zserver/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+@api_view(["GET"])
+def health_check(request):
+    return Response(
+        {
+            "status": "healthy",
+            "service": "my-api"
+        },
+        status=status.HTTP_200_OK
+    )
